54e942be7d9882320ed7819ac9f02686eccdd5b2
Emanuel Zúñiga Infante <emanuzuniga@gmail.com>
2016-08-18 20:33:02 -0600

    0.0.2 Returned to original Admin
    Returned to original Admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin
 
-# Register your models here.
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from.models import Product
+from.models import ProductDepartment
+from.models import ProductSubDepartment
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_code', 'product_barcode', 'product_description', 'product_department', 'product_subdepartment',
+                    'product_price', 'product_usetaxes', 'product_taxes')
+
+    search_fields = ('id', 'product_code', 'product_description', 'product_department__productdepartment_name',
+                     'product_subdepartment__productsubdepartment_name', 'product_price', 'product_unit',
+                     'product_usetaxes', 'product_taxes')
+#   filter_horizontal = ('bill_product_list',)
+
+
+@admin.register(ProductDepartment)
+class ProductDepartmentAdmin(admin.ModelAdmin):
+    list_display = ('productdepartment_code', 'productdepartment_name',)
+    search_fields = ('productdepartment_code', 'productdepartment_name',)
+
+
+@admin.register(ProductSubDepartment)
+class ProductSubDepartmentAdmin(admin.ModelAdmin):
+    list_display = ('productsubdepartment_name', 'productsubdepartment_department', 'productsubdepartment_code', )
+    search_fields = ('productsubdepartment_code', 'productsubdepartment_name',
+                     'productsubdepartment_department__productdepartment_name',)
